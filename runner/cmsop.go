c589af38429049a0d11e99f6f489a2e6ad2d29db
Bo Maryniuk <bo@maryniuk.net>
2020-07-13 20:27:36 +0200

    Use new compiler factory API instead of low-level compiler construction

--- runner/cmsop.go
+++ runner/cmsop.go
@@ -1,60 +1,42 @@
 package wzd_runner
 
 import (
-	wzlib_utils "github.com/infra-whizz/wzlib/utils"
-
 	nanocms_runners "github.com/infra-whizz/wzcmslib/nanorunners"
 	nanocms_state "github.com/infra-whizz/wzcmslib/nanostate"
-	nanocms_compiler "github.com/infra-whizz/wzcmslib/nanostate/compiler"
+	wzlib_utils "github.com/infra-whizz/wzlib/utils"
 )
 
 type WzCMS struct {
-	stateIndex *nanocms_state.NanoStateIndex
+	//stateIndex *nanocms_state.NanoStateIndex
+	compiler *nanocms_state.StateCompiler
 }
 
 // NewWzCMS creates a CMS runner
 func NewWzCMS(path ...string) *WzCMS {
 	cms := new(WzCMS)
-	cms.stateIndex = nanocms_state.NewNanoStateIndex().AddStateRoots(path...).Index()
+	cms.compiler = nanocms_state.NewStateCompiler().Index(path...)
 
 	return cms
 }
 
 // Call a loaded and compiled state
 func (cms *WzCMS) localCall(meta *nanocms_state.NanoStateMeta) (int, string) {
-	var err error = nil
-	cmp := nanocms_compiler.NewNstCompiler()
-	if err = cmp.LoadFile(meta.Path); err != nil {
-		return wzlib_utils.EX_GENERIC, err.Error()
-	}
-
-	// Load the entire chain of the local caller
-	for {
-		cMeta, _ := cms.stateIndex.GetStateById(cmp.Cycle())
-		if cMeta != nil {
-			if err = cmp.LoadFile(cMeta.Path); err != nil {
-				return wzlib_utils.EX_GENERIC, err.Error()
-			}
-		} else {
-			break
-		}
-	}
-
-	state := nanocms_state.NewNanostate()
-	if err := state.Load(cmp.Tree()); err != nil {
-		return wzlib_utils.EX_GENERIC, err.Error()
+	retcode, err := cms.compiler.Compile(meta.Path)
+	if err != nil {
+		return retcode, err.Error()
 	}
 
 	localRunner := nanocms_runners.NewLocalRunner()
-	localRunner.AddStateRoots(cms.stateIndex.GetStateRoots()...)
-	localRunner.Run(state)
+	localRunner.AddStateRoots(cms.compiler.GetStateIndex().GetStateRoots()...)
+	localRunner.Run(cms.compiler.GetState())
 	return localRunner.Errcode(), localRunner.Response().PrettyJSON()
+
 }
 
 // OfflineCallById state by the Id from the completely downloaded state tree.
 // If some reference files aren't there, this call supposed to fail.
 func (cms *WzCMS) OfflineCallById(stateId string) (int, string) {
-	meta, err := cms.stateIndex.GetStateById(stateId)
+	meta, err := cms.compiler.GetStateIndex().GetStateById(stateId)
 	if err != nil {
 		return wzlib_utils.EX_UNAVAILABLE, err.Error()
 	}
